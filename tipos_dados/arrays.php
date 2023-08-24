ee4261abad4501917f81c6c9c69b4f64f099cc98
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-24 19:30:02 -0300

    Arrays

---
+++ tipos_dados/arrays.php
@@ -0,0 +1,33 @@
+<?php
+
+$a = [1, 2, 3, 4, 5];
+echo $a; // erro ao converter array de dados para string
+
+echo '<br><br>';
+
+$b = [1, 2, 3, 4, 5];
+
+print_r($b); // imprime o array de inteiros
+
+echo '<br><br>';
+
+$c = ['paulo', 'luisa', 'joao', 'maria'];
+
+print_r($c); // imprime o array de strings
+
+echo '<br><br>';
+
+$d = [1, 2, 3, 4, 5];
+echo $a[2] . '<br><br>'; // imprime o indice 2 do array
+
+$e = ['Paulo', 1];
+
+print_r($e);
+
+echo '<br><br>';
+
+$e = ['Paulo', 1];
+
+print_r($e[1]);
+
+
