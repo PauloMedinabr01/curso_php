2317876d1780affa7a2ad1714efa4e1372346fca
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-24 19:04:57 -0300

    Ajustes strings

--- tipos_dados/strings.php
+++ tipos_dados/strings.php
@@ -10,3 +10,19 @@ echo '<br>';
 
 $numero = 1250;
 echo "Texto com aspas duplas e impressão da variável número = $numero ."; //imprime texto e variáveis
+
+echo '<br>';
+
+if (is_string("Paulo")) {
+    echo "É uma string. <br>";
+}
+
+if (is_string(2)) {
+    echo "É uma string. <br>";
+}
+
+$nome = "Paulo";
+
+if (is_string($nome)) {
+    echo "É uma string. <br>";
+}
