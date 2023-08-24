a782e46f8908e5043e68aebcd1bd99d7c28b33be
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-24 19:00:49 -0300

    Checando strings

---
+++ tipos_dados/strings.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'Isso é um texto com aspas simples.'; //imprime somente texto
+
+echo '<br>';
+
+echo "Isso é um texto com aspas duplas."; //imprime texto e variáveis
+
+echo '<br>';
+
+$numero = 1250;
+echo "Texto com aspas duplas e impressão da variável número = $numero ."; //imprime texto e variáveis
