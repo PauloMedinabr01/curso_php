29400783cfd78755b6f67b00c2dcc93c9948ba6e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-24 20:20:09 -0300

    Trabalhando com variáveis

---
+++ variaveis/criando_variaveis.php
@@ -0,0 +1,27 @@
+<?php
+
+$nome = 'Paulo';
+$sobrenome = "Coelho";
+$idade = 41;
+$altura = 1.80;
+
+echo 'Nome: ' . $nome . ' ' . $sobrenome . ', ' . 'idade ' . $idade . ' anos, altura ' . $altura;
+
+$velocidade = 100;
+echo '<br>';
+
+$marca = 'Ferrari';
+echo '<br>';
+
+$itens = ["Teto solar", "Motor 3.0", "cor vermelha"];
+echo '<br>';
+
+echo $velocidade . '<br>';
+echo $marca . '<br>';
+echo '<br>';
+print_r($itens);
+
+
+
+
+
