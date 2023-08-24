4bba12605d14e4f8415cdfa5823efb4b762f81fd
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-24 19:19:43 -0300

    Checando booleanos

--- tipos_dados/booleans.php
+++ tipos_dados/booleans.php
@@ -1,3 +1,30 @@
 <?php
 
+echo "True imprime " . true . "<br>";
+echo '<br>';
 
+echo "False imprime 0 ou não exibe nada" . false . "<br>";
+echo '<br>';
+
+if (true) { //true
+    echo 'Verdadeiro <br>';
+}
+
+echo '<br>';
+
+if (5 > 2) { // true
+    echo 'Verdadeiro <br>';
+}
+echo '<br>';
+
+$a = true;
+if (is_bool($a)) {
+    echo 'É um booleano <br>';
+}
+
+echo '<br>';
+
+$b = false;
+if (is_bool($a)) {
+    echo 'É um booleano <br>';
+}
