afcc4d9c263a11befe4a74036440ac564eabf00b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 20:46:40 -0300

    Testando estrutura WHILE

---
+++ 5_estruturas_repeticao/estrutura_while.php
@@ -0,0 +1,43 @@
+<?php
+
+// definição do contador
+$x = 0;
+
+// definição da estrutura
+while ($x < 10) {
+
+    // corpo do loop
+    echo "Valor de x: $x <br><br>";
+
+    // incrementando o contador
+    $x++;
+}
+
+echo "Continuando o código <br><br>";
+
+$x = 0;
+
+// definição da estrutura
+while ($x < 10) {
+
+    // corpo do loop
+    echo "Valor de x: $x <br><br>";
+
+    // incrementando o contador
+    $x = $x + 2;
+}
+
+echo "Continuando o código <br><br>";
+
+$x = 10;  // Inicialização com o valor de início desejado
+
+// definição da estrutura
+while ($x > 0) {
+
+    // corpo do loop
+    echo "Valor de x: $x <br><br>";
+
+    // decrementando o contador
+    $x--;
+}
+
