4423ed1f40b9719b484f73399e81ea94043f201c
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 17:44:13 -0300

    Operador ternário

---
+++ 3_expressoes_operadores/operador_ternario.php
@@ -0,0 +1,19 @@
+<?php
+
+echo 5 > 2 ? "5 maior que 2" : "5 menor que 2";
+echo "<br><br>";
+
+echo 5 < 2 ? "5 maior que 2" : "5 menor que 2";
+echo "<br><br>";
+
+$a = 5;
+$b = 10;
+
+echo $a <= $b ? "a menor ou igual que b" : "a maior ou igual que b";
+echo "<br><br>";
+
+$c = 25;
+$d = 10;
+
+echo $c >= $d ? "c maior ou igual que d" : "a menor ou igual que b";
+echo "<br><br>";
