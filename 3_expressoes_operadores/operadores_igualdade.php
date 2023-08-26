e9ba49c0130fb1b74480a3b248174176f5c1529b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 15:58:16 -0300

    Comparação de igualdade

---
+++ 3_expressoes_operadores/operadores_igualdade.php
@@ -0,0 +1,45 @@
+<?php
+
+if (1 == 1) {
+    echo "Comparação de números é verdadeira <br><br>";
+} else {
+    echo "Comparação é falsa <br><br>";
+}
+
+if (3 == 4) {
+    echo "Comparação de números é verdadeira <br><br>";
+} else {
+    echo "Comparação de números é falsa <br><br>";
+}
+
+$a = 12;
+$b = 12;
+$c = 100;
+
+if ($a == $b) {
+    echo "Comparação de variáveis verdadeira <br><br>";
+} else {
+    echo "Comparação de variáveis é falsa <br><br>";
+}
+
+if ($a == $c) {
+    echo "Comparação de variáveis é verdadeira <br><br>";
+} else {
+    echo "Comparação de variáveis é falsa <br><br>";
+}
+
+$nome = "Paulo";
+$aluno = "Paulo";
+if ($nome == $aluno) {
+    echo "Comparação de strings é verdadeira <br><br>";
+} else {
+    echo "Comparação de strings é falsa <br><br>";
+}
+
+$nome = "Paulo";
+$aluno = 10;
+if ($nome == $aluno) {
+    echo "Comparação de strings é verdadeira <br><br>";
+} else {
+    echo "Comparação de strings é falsa <br><br>";
+}
