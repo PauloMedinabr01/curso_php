be2732f15f710e6ac94df2baae0781432e814757
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 17:20:51 -0300

    Operador Cast

---
+++ 3_expressoes_operadores/operador_cast.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = (int)"12";
+
+echo $a;
+echo "<br><br>";
+
+echo $a + 10;
+echo "<br><br>";
+
+
+if ($a === 12) {
+    echo "A é identico a 12 <br><br>";
+}
+
+$c = (string)34;
+
+echo $c;
+echo "<br><br>";
+
+if ($c === '34') {
+    echo "Verdadeiro";
+}
