83327f4f7190544b3aedf3faa3af3e268cb2466e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 16:26:05 -0300

    Operador maior ou igual e maior

---
+++ 3_expressoes_operadores/operador_maior_ou_igual_e_maior.php
@@ -0,0 +1,29 @@
+<?php
+
+if (5 > 2) {
+    echo " 5 maior que 2 <br><br>";
+} else {
+    echo " 5 não é maior que 2 <br><br>";
+}
+
+$a = 5;
+$b = 10;
+$c = 5;
+
+if ($a > $b) {
+    echo " a maior que b <br><br>";
+} else {
+    echo " a não é maior que b <br><br>";
+}
+
+if ($a >= $b) {
+    echo " a maior que b <br><br>";
+} else {
+    echo " a não é maior que b <br><br>";
+}
+
+if ($a >= $c) {
+    echo " a maior ou igual que c <br><br>";
+} else {
+    echo " a não é maior ou igual que c <br><br>";
+}
