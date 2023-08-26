385908584ffa5e19f061978d3bffe33027f8c5bf
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 17:12:36 -0300

    Operador NOT

---
+++ 3_expressoes_operadores/operador_not.php
@@ -0,0 +1,13 @@
+<?php
+
+if (5 > 2) {
+    echo "Verdadeiro <br><br>";
+} else {
+    echo "Falso <br><br>";
+}
+
+if (!(5 > 2)) {
+    echo "Verdadeiro <br><br>";
+} else {
+    echo "Falso <br><br>";
+}
