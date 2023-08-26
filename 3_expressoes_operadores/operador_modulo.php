f2e74f3c81b6c09cf322db3cbf00c3e23904c802
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 15:27:09 -0300

    Operador modulo (resto da divisão)

---
+++ 3_expressoes_operadores/operador_modulo.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 12 % 2;
+echo " Módulo 12 % 2 = $a <br><br>";
+
+$b = 12 / 2;
+echo " Divisão 12 / 2 = $b <br><br>";
+
+$a = 13 % 2;
+echo " Módulo 13 % 2 = $a <br><br>";
+
+$a = 13 % 5;
+echo " Módulo 13 % 5 = $a <br><br>";
