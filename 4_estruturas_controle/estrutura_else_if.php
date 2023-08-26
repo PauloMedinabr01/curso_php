388e041c2c9ca45a4e5f869cba8aa298e27ffe11
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 20:12:34 -0300

    Testando estrutura ELSE IF

---
+++ 4_estruturas_controle/estrutura_else_if.php
@@ -0,0 +1,25 @@
+<?php
+
+if (5 > 2) {
+    echo "entrou no IF 1 <br><br>";
+} elseif (10 < 5) {
+    echo "entrou no ELSE IF 1 <br><br>";
+} else {
+    echo "entrou no ELSE 1 <br><br>";
+}
+
+if (5 < 2) {
+    echo "entrou no IF 2 <br><br>";
+} elseif (10 > 5) {
+    echo "entrou no ELSE IF 2 <br><br>";
+} else {
+    echo "entrou no ELSE 2 <br><br>";
+}
+
+if (5 < 2) {
+    echo "entrou no IF 2 <br><br>";
+} elseif (10 < 5) {
+    echo "entrou no ELSE IF 2 <br><br>";
+} else {
+    echo "entrou no ELSE 2 <br><br>";
+}
