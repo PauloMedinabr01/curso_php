ea6bd4120804585e0f63f9aecad4ec88b07c941b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 19:58:03 -0300

    Testando estrutura IF

---
+++ 4_estruturas_controle/testando_if.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 19;
+$idade1 = 16;
+$idade2 = 23;
+
+$maioridade = 18;
+
+if ($idade >= $maioridade) {
+    echo "IF 1 Maior de idade <br><br>";
+}
+
+if ($idade1 >= $maioridade) {
+    echo "IF 2 Maior de idade <br><br>";
+}
+
+if ($idade2 >= $maioridade) {
+    echo "IF 3 Maior de idade <br><br>";
+}
