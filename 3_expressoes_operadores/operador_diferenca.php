4ea5894f3edb2977595cf49098ea6b500900fc2e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 16:10:43 -0300

    Operador de diferença

---
+++ 3_expressoes_operadores/operador_diferenca.php
@@ -0,0 +1,30 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = 5;
+
+if ($a != $b) {
+    echo "São diferentes <br><br>";
+} else {
+    echo "São diferentes <br><br>";
+}
+
+if ($a != $c) {
+    echo "São diferentes <br><br>";
+} else {
+    echo "São iguais <br><br>";
+}
+
+if (false != "Teste") {
+    echo "São diferentes <br><br>";
+} else {
+    echo "São iguais <br><br>";
+}
+
+if (3 != "3") {
+    echo "São diferentes <br><br>";
+} else {
+    echo "São iguais <br><br>";
+}
+
