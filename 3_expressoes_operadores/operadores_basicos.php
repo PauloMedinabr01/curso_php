47ac9868f66e43603fdc5b54377d4985fa66ae34
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 15:21:10 -0300

    Operadores básicos

---
+++ 3_expressoes_operadores/operadores_basicos.php
@@ -0,0 +1,17 @@
+<?php
+
+$soma = 10 + 5;
+echo "A soma é: $soma <br><br>";
+
+$subtracao = 10 - 5;
+echo "A subtracao é: $subtracao <br><br>";
+
+$multiplicacao = 10 * 5;
+echo "A multiplicação é: $multiplicacao <br><br>";
+
+$divisao = 20 / 5;
+echo "A divisao é: $divisao <br><br>";
+
+$op = 5 + (2 / 7);
+echo "O resultado de op é: $op <br><br>";
+
