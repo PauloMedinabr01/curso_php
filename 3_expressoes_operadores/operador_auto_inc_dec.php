9828d2c4ff73d7a2b9027378236d2994f5473f67
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 15:44:31 -0300

    Operadores auto incremento e decremento

---
+++ 3_expressoes_operadores/operador_auto_inc_dec.php
@@ -0,0 +1,21 @@
+<?php
+
+// Utilizados somente quando for necessário incrementar ou decrementar 1
+
+$x = 15;
+$y = 20;
+
+$x++;
+$y = $y + 1;
+
+echo " x: $x" . "<br>";
+echo " y: $y" . "<br>";
+
+$n = 15;
+$m = 20;
+
+$x--;
+$y = $y - 1;
+
+echo " x: $x" . "<br>";
+echo " y: $y" . "<br>";
