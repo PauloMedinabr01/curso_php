c2b61df4455a871bbee6bb02558a28917279e8e7
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-27 10:01:12 -0300

    Exibição de conteúdo

---
+++ 6_inclusao_de_codigo/exibicao_de_conteudo/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$nome = "Paulo";
+$sobrenome = "Coelho"
+
+?>
+
+<?= $teste = "ads"; ?>
+
+<form action="">
+    <div>
+        <label>
+            <input type="text" value="<?= $nome; ?>">
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="text" value="<?= $sobrenome; ?>">
+        </label>
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
