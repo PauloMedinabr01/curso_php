dcc2cf6548bfd882fc2d8458e66913aa02e09584
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-26 22:01:55 -0300

    Testando estrutura DO WHILE

---
+++ 5_estruturas_repeticao/estrutura_do_while.php
@@ -0,0 +1,23 @@
+<?php
+
+$j = 0;
+
+do {
+    echo "Testando o do while: $j <br><br>";
+
+    $j++;
+
+} while ($j < 10);
+
+echo "Outro loop <br><br>";
+
+$i = 10;
+
+do {
+    echo "Testando o do while 2: $i <br><br>";
+
+    $i--;
+
+} while ($i > 0);
+
+echo "Outro loop <br><br>";
