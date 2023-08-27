5ba1854b01861e1d79acc2fde4c858083cb9f5dc
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-27 11:04:30 -0300

    Criando e chamando funções

---
+++ 7_funcoes/funcao_somar.php
@@ -0,0 +1,15 @@
+<?php
+
+// função somar
+function somar()
+{
+    $a = 10;
+    $b = 15;
+    echo "Soma de a + b feita pela função, resultado: " . $a + $b . "<br><br>";
+}
+
+somar();
+
+// função do php
+echo strtoupper("testando função");
+
