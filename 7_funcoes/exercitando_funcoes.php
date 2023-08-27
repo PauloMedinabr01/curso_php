bf020a0907d99cbb0346bd738fba81048aea198b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-27 11:14:39 -0300

    Exercitando funções

---
+++ 7_funcoes/exercitando_funcoes.php
@@ -0,0 +1,27 @@
+<?php
+
+$array = ["Teste", "Balão", "Olá", "Janela", "Planta"];
+
+// função para imprimir o array
+print_r($array);
+
+echo "<br><br>";
+
+// junta um array em string unica, o separador é uma virgula definido nos parametros
+$teste = implode(", ", $array);
+
+echo $teste . "<br><br>";
+
+// criar uma função com 3 variáveis númericas e exibir a multiplicação com um echo
+function multiplicar(): void
+{
+    $a = 3;
+    $b = 2;
+    $c = 4;
+
+    echo $a * $b * $c;
+}
+
+multiplicar();
+
+
