53f8a8aab7d59c01694884e5f53416f25e4aa9d2
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 20:51:03 -0300

    Interpolação de variáveis.

---
+++ 8_strings/interpolar_variaveis.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = "Paulo";
+$idade = 29;
+$profissao = "Dev";
+
+echo "Minha idade é $idade anos. <br><br>";
+
+echo "Minha idade é {$idade} anos. <br><br>";
+
+echo "Olá, meu nome é {$nome}, minha idade é {$idade} anos e minha profissão é {$profissao}. <br><br>";
