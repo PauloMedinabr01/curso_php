ab494f9016486821613ebd844d02c264862f8945
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 21:50:21 -0300

    Função printf

---
+++ 8_strings/funcao_printf.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "Paulo";
+
+// %s -> string
+printf("O nome é %s. <br><br>", $nome);
+
+$numero = 10;
+
+// %d -> int
+printf("O nome é %d. <br><br>", $numero);
+
+$temperatura = 18.58;
+
+// %f -> int - limitar casas decimais %.2f - duas casas decimáis
+printf("A temperatura atual é %.2f graus. <br><br>", $temperatura);
+
+
