7c1b5808fc649063ca7338c51691f4732f1648a9
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 22:31:44 -0300

    Alterando o case de palavras com ucfirst e ucwords

---
+++ 8_strings/alterando_o_case_palavras.php
@@ -0,0 +1,11 @@
+<?php
+
+$str1 = "testando o case de uma palavra com ucfirst. <br><br>";
+
+//primeira letra em maiusculo
+echo ucfirst($str1);
+
+//primeira letra de cada palavra em maiusculo
+$str2 = "testando o case de uma palavra com ucwords. <br><br>";
+
+echo ucwords($str2);
