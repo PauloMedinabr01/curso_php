e0da80914767f0e25e67e6d84299c265c2555e4e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 22:26:58 -0300

    Alterando o case com strtoupper e strtolower

---
+++ 8_strings/alterando_o_case.php
@@ -0,0 +1,9 @@
+<?php
+
+$str1 = " esta string esta originalmente em caixa baixa. <br><br>";
+
+echo strtoupper($str1);
+
+$str2 = " ESTA STRING ESTA ORIGINALMENTE EM CAIXA ALTA. <br><br>";
+
+echo strtolower($str2);
