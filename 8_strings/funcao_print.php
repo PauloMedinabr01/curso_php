3458d4b6e7c62d1939ed38f2a7091eb6de6f1f83
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 21:31:18 -0300

    Função print

---
+++ 8_strings/funcao_print.php
@@ -0,0 +1,7 @@
+<?php
+
+print ("Imprimindo texto com a função print. <br><br>");
+
+$carro = "BMW";
+
+print ("Imprimindo texto com interpolação de variaveis: variável \$carro = $carro. <br><br>");
