d7845800cafd15571b2b3365246941ea5fdd800a
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 21:05:22 -0300

    Valores de escape

---
+++ 8_strings/valores_escape.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Content-type: text/plain");
+
+echo "Isso vai ficar na primeira linha \n e Isso na segunda linha \n";
+
+echo "Testando o tab \t aqui \n";
+
+echo "Testando a barra invertida \\ \n";
+
+echo "Imprimindo o dólar \$  \n";
+
+
