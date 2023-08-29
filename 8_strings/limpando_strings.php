25872a9314c8bfb368bd2ddb3bf61308194a8fc3
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-08-28 22:22:49 -0300

    Limpando strings com rtrin e trim

---
+++ 8_strings/limpando_strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$str1 = "     Paulo   ";
+
+echo "Está é a string 1: $str1 . <br><br>";
+
+$str1Limpa = trim($str1);
+
+echo "Está é a string 1: $str1Limpa. <br><br>";
+
+$str1Limpa2 = rtrim($str1);
+
+echo "Está é a string 1: $str1Limpa2. <br><br>";
