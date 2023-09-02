e03c16f69b0b7e29558213559fd7cd16c49bc030
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-02 10:49:41 -0300

    Repetindo strings com str_repeat

---
+++ 8_strings/repeticao_strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$str1 = "Meu nome é Paulo. ";
+
+$strRep = str_repeat($str1, 5); // informar a variável e a quantidade de vezes que a string será repetida.
+
+echo "$strRep <br><br>";
+
+$str2 = "Meu nome é João. ";
+
+echo str_repeat($str2, 3);
