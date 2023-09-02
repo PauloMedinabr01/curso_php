a6017386364db61d47f6f6949f8f7be321a70912
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-02 10:58:24 -0300

    Separando strings com explode

---
+++ 8_strings/converter_string_array.php
@@ -0,0 +1,13 @@
+<?php
+
+$string = "www.teste.com.br";
+
+$stringToArray = explode(".", $string); // define o separador, informar a variável
+
+print_r($stringToArray); //print_r imprime o array
+echo "<br><br>";
+
+$stringToArray2 = explode(",", $string); // define o separador, informar a variável
+
+print_r($stringToArray2); //print_r imprime um item uníco por não encontrar o separador definido
+echo "<br><br>";
