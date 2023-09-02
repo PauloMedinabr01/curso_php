dd5dbbf33bb977bed286726c60f1fdf7f635c06f
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-02 10:37:37 -0300

    Resgatando parte de strings com substr

---
+++ 8_strings/resgatando_palavra.php
@@ -0,0 +1,11 @@
+<?php
+
+$string = "Cade meu queijo, ele estava aqui em cima";
+
+$queijo = substr($string, 9, 6); //informar a variável, indice que será feito a captura, tamanha da string a recortar
+
+echo "$queijo <br><br>";
+
+$estava = substr($string, 21, 6); //informar a variável, indice que será feito a captura, tamanha da string a recortar
+
+echo "$estava <br><br>";
