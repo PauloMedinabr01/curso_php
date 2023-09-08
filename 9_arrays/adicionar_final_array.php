7d1c17c940aea2dbe2b7d456fef9c455570ef9de
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-07 21:23:34 -0300

    Adicionar item no fim do array

---
+++ 9_arrays/adicionar_final_array.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [1, 2, 3];
+print_r($arr);
+echo "<br><br>";
+
+$arr [] = 4;
+print_r($arr);
+echo "<br><br>";
+
+$arr = [];
+
+$arr ['carro'] = 'testando';
+print_r($arr);
+echo "<br><br>";
