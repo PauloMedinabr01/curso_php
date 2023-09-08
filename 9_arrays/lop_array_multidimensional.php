6d21ba791cb74374aec795baa750fdfbe14cb219
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-07 22:07:04 -0300

    Array multidimensional com loops

---
+++ 9_arrays/lop_array_multidimensional.php
@@ -0,0 +1,18 @@
+<?php
+
+$arr = [
+    [1, 2, 3, 4],
+    [2, 4, 6, 8],
+    [3, 6, 9, 12],
+];
+
+for ($i = 0; $i < count($arr); $i++) {
+
+    echo "imprimindo array externo: " . ($i + 1) . "<br>";
+
+    for ($j = 0; $j < count($arr[$i]); $j++) {
+
+        echo $arr[$i][$j] . "<br>";
+
+    }
+}
