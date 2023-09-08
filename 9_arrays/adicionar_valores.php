95a84b775f6e6f4f2b3c70fe35ef12f93bbd223f
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-07 21:17:53 -0300

    Decompondo urls com parse_url

---
+++ 9_arrays/adicionar_valores.php
@@ -0,0 +1,26 @@
+<?php
+
+$arr = [];
+
+print_r($arr);
+echo "<br><br>";
+
+$arr = [10, 20, 30];
+
+print_r($arr);
+echo "<br><br>";
+
+$arr [5] = 10;
+
+print_r($arr);
+echo "<br><br>";
+
+$arr [1] += 55;
+
+print_r($arr);
+echo "<br><br>";
+
+$arrAssoc ["Carro"] = 'BMW';
+
+print_r($arrAssoc);
+echo "<br><br>";
