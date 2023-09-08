986c490ce3a7e827178752bcb04152f6d11f300f
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-07 21:27:29 -0300

    Criando array rapidamente

---
+++ 9_arrays/range_array.php
@@ -0,0 +1,21 @@
+<?php
+
+$arr = range(1, 10);
+
+print_r($arr);
+echo "<br><br>";
+
+$arr = range(5, 50);
+
+print_r($arr);
+echo "<br><br>";
+
+$arr = range(0, 100, 10);
+
+print_r($arr);
+echo "<br><br>";
+
+$arr = range(1, 1000, 100);
+
+print_r($arr);
+echo "<br><br>";
