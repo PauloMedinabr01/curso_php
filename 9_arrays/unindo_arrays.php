4150107385b8ebc974343fdfcd337a95d4f79f65
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-09 13:56:43 -0300

    Somando arrays

---
+++ 9_arrays/unindo_arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+// Definindo três arrays diferentes
+$arr1 = [1, 2, 3];
+$arr2 = [10, 5, 36, 71];
+$arr3 = [23, 60, 45, 78];
+
+// Mesclando os três arrays em um único array
+$arrayMerge = array_merge($arr1, $arr2, $arr3);
+
+// Imprimindo o resultado da mesclagem
+print_r($arrayMerge);
