3b0d4dd83f5774dcf85b7705c16c5cf4468732e3
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-09 13:28:57 -0300

    Ordenação de arrays com sort e rsort

---
+++ 9_arrays/ordenar_arrays.php
@@ -0,0 +1,32 @@
+<?php
+
+$arr = [1, 3, 5, 4, 8, 7, 2, 6, 9];
+
+print_r($arr);
+echo "<br><br>";
+
+sort($arr);
+
+print_r($arr);
+echo "<br><br>";
+
+rsort($arr);
+
+print_r($arr);
+echo "<br><br>";
+
+$arr2 = ['paulo', 'maria', 'luis', 'joao'];
+
+print_r($arr2);
+echo "<br><br>";
+
+sort($arr2);
+
+print_r($arr2);
+echo "<br><br>";
+
+rsort($arr2);
+
+print_r($arr2);
+echo "<br><br>";
+
