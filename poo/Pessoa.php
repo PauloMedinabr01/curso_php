8247dbb6ec0f6b6d037de774ff8a4a5a520f444e
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-09-09 19:36:24 -0300

    Instanciando vários objetos a partir de uma classe

--- poo/Pessoa.php
+++ poo/Pessoa.php
@@ -6,9 +6,22 @@ class Pessoa
 {
     function falar(): void
     {
-        echo "Método falar da classe pessoa.";
+        echo "Método falar da classe pessoa. <br><br>";
+    }
+
+    function somar($x, $y): void
+    {
+        echo "Método somar da classe Pessoa. A soma de x e y é: " . $x + $y . "<br><br>";
     }
 }
 
 $pessoa = new Pessoa();
 $pessoa->falar();
+
+$joao = new Pessoa();
+$joao->falar();
+
+$funcionario = new Pessoa();
+$funcionario->falar();
+
+$pessoa->somar(2, 6);
